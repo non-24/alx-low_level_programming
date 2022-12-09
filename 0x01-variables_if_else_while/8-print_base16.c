6e936a81fcc0a9feeb4700994c48412f5bc7011f
non-24 <nsoba6@gmil.com>
2022-12-09 15:01:12 -0800

    my task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,8 @@
 /**
  * main - main block
  * Description: print all the numbers of base 16 in lowercase,
- *  followed by a new line
+ * followed by a new line
+ * Return: 0
  */
 int main(void)
 {
