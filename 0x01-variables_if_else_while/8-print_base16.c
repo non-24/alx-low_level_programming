859bc4cd49412c3f7a7dff50fdf1b0d6547c24fd
non-24 <nsoba6@gmil.com>
2022-12-09 14:56:13 -0800

    my task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print all the numbers of base 16 in lowercase,
+ *  followed by a new line
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'' ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
