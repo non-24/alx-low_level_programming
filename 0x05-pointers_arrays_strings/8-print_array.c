f37e63a091ece66518254b8684f064a9a371dd9b
non-24 <nsoba6@gmil.com>
2022-12-19 13:55:12 -0800

    my task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,16 +8,13 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int i = 0;
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != n - 1)
-		{
-			printf(",");
-			printf(" ");
-		}
+		if (i < n - 1)
+			printf(", ");
 	}
 	printf("\n");
 }
