b94ad9b766e2d5fe67007e3598ff62930e5d4821
non-24 <nsoba6@gmil.com>
2022-12-19 13:24:16 -0800

    my task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,24 +7,22 @@
  */
 void rev_string(char *s)
 {
-	int string = 0, a, b;
-	char *str, temp;
+	int i, j, k, temp;
 
-	while (string > 0)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (s[string]	'\0')
-			break;
-		string++;
+		i++;
 	}
-	str s;
+	k = 0;
+	j = i - 1;
 
-	for (a	0; a < (string - 1); a++)
+	while (k < j)
 	{
-		for (b	a + 1; b > 0; b--)
-		{
-			temp	* (str + j);
-			*(str + b)	* (str + (b - 1));
-			*(str + (b - 1))	temp;
-		}
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp;
+		k++;
+		j--;
 	}
 }
