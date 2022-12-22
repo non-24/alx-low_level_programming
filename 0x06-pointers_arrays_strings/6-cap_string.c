96492a541f59b03ee5bc315aeafdd354bd55ec1f
non-24 <nsoba6@gmil.com>
2022-12-22 12:21:55 -0800

    my task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,36 +2,35 @@
 
 /**
  * cap_string - a function that capitalizes all words of a string
- * @s: string to capitalize
+ * @str: string to capitalize
  * Return: the capitalized string
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int x, y;
-	int trigger;
-	char nots[] = ",;.!?(){}\nt\" ";
+	int i = 0;
 
-	for (x = 0, trigger = 0; s[x] != '\0'; x++)
+	while (str[i])
 	{
-		if (s[0] > 96 && s[0] < 123)
-			trigger = 1;
-		for (y = 0; nots[y] != '\0'; y++)
-		{
-			if (nots[y] == s[x])
-				trigger = 1;
-		}
-		if (trigger)
-		{
-			if (s[x] > 96 && s[x] < 123)
-			{
-				s[x] -= 32;
-				trigger = 0;
-			}
-			else if (s[x] > 64 && s[x] < 91)
-				trigger = 0;
-			else if (s[x] > 47 && s[x] < 58)
-				trigger = 0;
-		}
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
+			str[i] -= 32;
+		i++;
 	}
-	return (s);
+
+	return (str);
 }
