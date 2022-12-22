7e0f65fa3277eb752034f095a4aa75c6e4e18c83
non-24 <nsoba6@gmil.com>
2022-12-22 12:05:43 -0800

    my task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,32 +2,36 @@
 
 /**
  * cap_string - a function that capitalizes all words of a string
- * @string: string to capitalize
+ * @s: string to capitalize
  * Return: the capitalized string
  */
-char *cap_string(char *string)
+char *cap_string(char *s)
 {
-	int i = 0;
+	int x, y;
+	int trigger;
+	char nots[] = ",;.!?(){}\nt\" ";
 
-	while (string[++i])
+	for (x = 0, trigger = 0; s[x] != '\0'; x++)
 	{
-		while (!(string[i] >= 'a' && string[i] <= 'z'))
-			i++;
-
-		if (string[i - 1] == ' ' ||
-				string[i - 1] == '\t' ||
-				string[i - 1] == '\n' ||
-				string[i - 1] == ',' ||
-				string[i - 1] == ';' ||
-				string[i - 1] == '.' ||
-				string[i - 1] == '!' ||
-				string[i - 1] == '?' ||
-				string[i - 1] == '"' ||
-				string[i - 1] == '(' ||
-				string[i - 1] == ')' ||
-				string[i - 1] == '{' ||
-				string[i - 1] == '}')
-			string[i] -= 32;
+		if (s[0] > 96 && s[0] < 123)
+			trigger = 1;
+		for (y = 0; nots[y] != '\0'; y++)
+		{
+			if (nots[y] == s[x])
+				trigger = 1;
+		}
+		if (trigger)
+		{
+			if (s[x] > 96 && s[x] < 123)
+			{
+				s[x] -= 32;
+				trigger = 0;
+			}
+			else if (s[x] > 64 && s[x] < 91)
+				trigger = 0;
+			else if (s[x] > 47 && s[x] < 58)
+				trigger = 0;
+		}
 	}
-	return (string);
+	return (s);
 }
